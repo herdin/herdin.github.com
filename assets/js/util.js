87503111fafbeff22479c06cffe126b15e660f7c
epu_baal_laptop <herdin86@gmail.com>
2020-01-31 13:59:10 +0900

    edit post/module

--- assets/js/util.js
+++ assets/js/util.js
@@ -12,8 +12,42 @@
 define(['jquery'], ($) => {
     return {
       genID : () => 'generatedID-' + Math.floor(Math.random()*100000),
-      genTable : (tableHeaders, tableDatas) => {
-        console.log('loaded jquery version -> ' +$.fn.jquery);
+
+      genPureTable : (table_location_indicator_id, table_headers, table_datas) => {
+        const prefix_TABLE = '<table class="pure-table">';
+      	const suffix_TABLE = '</table>';
+      	const prefix_THEAD = '<thead>';
+      	const suffix_THEAD = '</thead>';
+      	const prefix_TBODY = '<tbody>';
+      	const suffix_TBODY = '</tbody>';
+      	const prefix_TR = '<tr>';
+      	const suffix_TR = '</tr>';
+      	const prefix_TD = '<td>';
+      	const suffix_TD = '</td>';
+      	let tableTags = '';
+
+      	tableTags += prefix_TABLE;
+      	tableTags += prefix_THEAD;
+      	tableTags += prefix_TR;
+      	for(let colindex in table_headers) {
+          tableTags += prefix_TD + table_headers[colindex] + suffix_TD;
+      	}
+      	tableTags += suffix_TR;
+      	tableTags += suffix_THEAD;
+
+      	tableTags += prefix_TBODY;
+      	for(let rowindex in table_datas) {
+      		tableTags += prefix_TR;
+      	  for(let colindex in table_datas[rowindex]) {
+      		tableTags += prefix_TD + table_datas[rowindex][colindex] + suffix_TD;
+      	  }
+      	  tableTags += suffix_TR;
+      	}
+      	tableTags += suffix_TBODY;
+      	tableTags += suffix_TABLE;
+
+      	$('#' + table_location_indicator_id).after(tableTags);
+
       },
     };
 });
